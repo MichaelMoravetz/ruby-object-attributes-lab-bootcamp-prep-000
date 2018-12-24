aa4eb614f8797172ced545073baa26993d6cef62
Michael Moravetz <moravemv@gmail.com>
2018-12-24 21:32:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,5 @@
 class Dog 
   
-  def initialize(name, breed)
+  def initialize(name)
     @name = name
-    @breed = breed
-  end
-  
-  def name=(name)
-    @name = name 
-  end
-  
-  def name
-    "#{name}"
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    "#{breed}"
-  end
 end
